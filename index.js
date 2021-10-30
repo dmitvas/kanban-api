0dedd093f2e1f36b6e89fe0c70b432c86e7d347b
dzmitryvasileuski <dzmitry.vasileuski@gmail.com>
2021-10-30 12:31:56 +0300

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); // import express from 'express'
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // parse application/x-www-form-urlencoded
